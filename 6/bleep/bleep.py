1d1b78886d407eab64d148a0d01cf5e51f1e4cd7
yshalsager <ysh-alsager@hotmail.com>
2019-07-17 14:40:50 +0200

    pset6: bleep: solution

--- 6/bleep/bleep.py
+++ 6/bleep/bleep.py
@@ -3,8 +3,25 @@ from sys import argv
 
 
 def main():
-
-    # TODO
+    # Ensure that command line argument is one
+    if len(argv) != 2:
+        print('Usage: python bleep.py dictionary')
+        exit(1)
+    words = set()
+    # load words from file
+    with open(argv[1], 'r') as dictionary:
+        for item in dictionary:
+            words.add(item.strip())
+    text = get_string("What message would you like to censor?\n")
+    # censor the message
+    censored = []
+    for word in text.split(" "):
+        if word.lower() in words:
+            censor = ''.join(['*' for c in word])
+            censored.append(censor)
+        else:
+            censored.append(word)
+    print(' '.join(censored))
 
 
 if __name__ == "__main__":
